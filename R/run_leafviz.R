bab7994c07a113e2145de72b3f5d762a5678f76a
Jack Humphrey <jack@Jacks-MacBook-Pro-3.local>
2021-02-08 17:52:36 -0500

    fixed bug that stopped leafviz running in RStudio, added fun startup message

--- R/run_leafviz.R
+++ R/run_leafviz.R
@@ -10,11 +10,16 @@
 leafviz <- function(infile = system.file("extdata/Brain_vs_Heart_results.Rdata", package = "leafviz") ){
 
   stopifnot(file.exists(infile))
+  message(" * Welcome to Leafviz! Loading data...")
   load(infile, envir = .GlobalEnv)
-  if (getOption("browser") == ""){
-    options(browser = "firefox")
+  message(" * running app!")
+  # if using Rstudio then getOption("browser") is a function for some reason
+  if(!is.function(getOption("browser") ) ){
+    # if using windows - default to firefox for now
+    if (getOption("browser") == ""){
+        options(browser = "firefox")
+    }
   }
-
   shiny::runApp( launch.browser=TRUE, appDir = system.file("application", package = "leafviz") )
 
 }
