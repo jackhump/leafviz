015cfdc4aa22b64f39dacca41e46423a69bd975b
Jack Humphrey <jack@jacks-mbp-3.home>
2021-06-01 10:59:49 -0400

    added report making function

--- R/run_leafviz.R
+++ R/run_leafviz.R
@@ -23,3 +23,41 @@ leafviz <- function(infile = system.file("extdata/Brain_vs_Heart_results.Rdata",
   shiny::runApp( launch.browser=TRUE, appDir = system.file("application", package = "leafviz") )
 
 }
+
+#' Write simple HTML report
+#'
+#' @param infile
+#' @param outfile
+#'
+#' @return
+#' @export
+#'
+#' @examples
+make_leafviz_report <- function(
+  infile =  system.file("extdata/Brain_vs_Heart_results.Rdata", package = "leafviz"),
+  outfile = file.path(getwd(), "report.html" ) ) {
+
+  if( !file.exists(infile)){
+    stop(" * infile doesn't exist")
+  }
+
+  if( !dir.exists(dirname(outfile))){
+    stop(" * path to outfile doesn't exist")
+  }
+
+  message(" * Loading data...")
+  load(infile, envir = .GlobalEnv)
+  message(" * Writing report to ", outfile)
+  # translate tables into HTML
+  report <- c( paste("<h1>", code, "</h1>" ) ,
+               stargazer::stargazer(sample_table,type = "html", summary = FALSE, rownames = FALSE,
+                                    title = "Samples" ),
+               "<p>",
+               stargazer::stargazer(intron_summary,type = "html", summary = FALSE, rownames = FALSE,
+                                    title = "Junction classification"),
+               "<p>",
+               stargazer::stargazer(cluster_summary,type = "html", summary = FALSE, rownames = FALSE,
+                                    title = "Cluster classification")
+  )
+  writeLines(report,con = outfile)
+}
